670bc333220a86c70b86a5c3757bef935873114b
Harsh8668 <choudharyharsh789@gmail.com>
2022-07-07 08:08:03 +0530

    Deploy on heruko App

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  baseUrl:"https://bookstore.incubation.bridgelabz.com/"
+  production: false,
+  // baseUrl:"https://bookstore.incubation.bridgelabz.com/"
 };
 
 /*
