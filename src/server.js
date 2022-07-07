9aba17f5b14febdfd9e73d004e1fd517574cd169
Harsh8668 <choudharyharsh789@gmail.com>
2022-07-07 08:54:21 +0530

    Deploy on heruko App

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/CFP_Bookstore'));
+app.use(express.static(__dirname + '/dist/bookstore'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/CFP_Bookstore/index.html'));
+res.sendFile(path.join(__dirname+'/dist/bookstore/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
