d87214065ede3e59de36b314032a2c8d72b98313
Harsh8668 <choudharyharsh789@gmail.com>
2022-07-08 06:18:13 +0530

    Deploy on heruko App

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+//Install express server
 const express = require('express');
-const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static('./dist/bookstore'));
-// Start the app by listening on the default
-// Heroku port
-app.get('/*',(req,res)=>
+const path = require('path');
 
-res.sendFile('index.html',{root:'dist/bookstore/'}))
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/bookstore'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/bookstore/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
