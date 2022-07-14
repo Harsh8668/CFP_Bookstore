0fe23c864b50f22f0db36975521335d50aee6724
Harsh8668 <choudharyharsh789@gmail.com>
2022-07-14 20:45:28 +0530

    Updated  the test cases

--- src/app/component/dashboard/dashboard.component.spec.ts
+++ src/app/component/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, async, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
 
@@ -15,8 +15,12 @@ describe('DashboardComponent', () => {
       imports: [RouterTestingModule, HttpClientTestingModule, MatMenuModule],
       declarations: [ DashboardComponent ]
     })
-    .compileComponents();
+    .compileComponents().then(() => {
+      fixture = TestBed.createComponent(DashboardComponent);
+      component = fixture.componentInstance;
+    });
   });
+
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
@@ -27,4 +31,60 @@ describe('DashboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render title in a h3 tag', () => {
+    const fixture = TestBed.createComponent(DashboardComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('span').textContent).toContain('Bookstore');
+  });
+
+  it('should render title in a h3 tag', () => {
+    const fixture = TestBed.createComponent(DashboardComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.cart').textContent).toContain('cart');
+  });
+
+  it('should', async(() => {
+    spyOn(component, 'home');
+  
+    let button = fixture.debugElement.nativeElement.querySelector('.text');
+    button.click();
+  
+    fixture.whenStable().then(() => {
+      expect(component.home).toHaveBeenCalled();
+    });
+  }));
+
+  it('should', fakeAsync(() => {
+    spyOn(component, 'home');
+  
+    let button = fixture.debugElement.nativeElement.querySelector('.text');
+    button.click();
+    tick();
+    expect(component.home).toHaveBeenCalled();
+  
+  }));
+
+  // it('should', fakeAsync(() => {
+  //   spyOn(component, 'searchTitle');
+  
+  //   let button = fixture.debugElement.nativeElement.querySelector('.search');
+  //   button.click();
+  //   tick();
+  //   expect(component.searchTitle).toHaveBeenCalled();
+  
+  // }));
+
+  // it('shoulds', fakeAsync(() => {
+  //   spyOn(component, 'myCart');
+  
+  //   let button = fixture.debugElement.nativeElement.querySelector('.mat-menu button.mat-menu-item');
+  //   button.click();
+  //   tick();
+  //   expect(component.myCart).toHaveBeenCalled();
+  
+  // }));
+
 });
--- src/app/component/getbooks/getbooks.component.spec.ts
+++ src/app/component/getbooks/getbooks.component.spec.ts
@@ -1,12 +1,11 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { GetbooksComponent } from './getbooks.component';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { FilterPipe } from 'src/app/Pipe/filter.pipe';
-
 
 
 describe('GetbooksComponent', () => {
@@ -30,4 +29,23 @@ describe('GetbooksComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should', async(() => {
+    spyOn(component, 'low');
+  
+    let button = fixture.debugElement.nativeElement.querySelector('mat-option');
+    button.click();
+  
+    fixture.whenStable().then(() => {
+      expect(component.low).toHaveBeenCalled();
+    });
+  }));
+
+  it('should render title in a h3 tag', () => {
+    const fixture = TestBed.createComponent(GetbooksComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.name').textContent).toContain('Books');
+  });
+  
 });
